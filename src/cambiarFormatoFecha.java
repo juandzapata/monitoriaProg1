561bb331c4ba745d42f2311ef0615d9ff4ab8b1b
juandzapata <juan.zapata22871@ucaldas.edu.co>
2023-12-29 01:40:45 -0500

    Prueba

--- src/cambiarFormatoFecha.java
+++ src/cambiarFormatoFecha.java
@@ -4,35 +4,7 @@ import java.util.Scanner;
 public class cambiarFormatoFecha {
     public static void main(String[] args) throws Exception {        
         
-
-        //Creación del Scanner para recibir una entrada por consola
-        Scanner entrada = new Scanner(System.in);        
-
-        //Declaración de variables
-        ArrayList<String> datosArrayList = new ArrayList<String>();
-        String nuevaLinea = "";
-        Boolean condicion = false;
-        int i = 0;
-        
-        while(!condicion){
-
-            if(datosArrayList.get(i).equals("/")){
-                condicion = true;
-            }else{
-                nuevaLinea = entrada.nextLine();
-                datosArrayList.add(nuevaLinea);           
-            }
-
-            i++;
-        }
-        entrada.close();
-    }
-
-    public static String convertirFecha(String fecha){
-        String fechaConvert = "";
-        String fechaArray[] = fecha.split("/");
-        fechaConvert = fechaArray[2] + fechaArray[0] + fechaArray[1];
-        return fechaConvert;
+        System.out.println("Ingrese la fecha en formato dd/mm/aaaa");
     }
 }
 
