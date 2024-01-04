83408573bc88e45059c2bb3d0a533c81ef58baa5
juandzapata <juan.zapata22871@ucaldas.edu.co>
2024-01-03 19:40:38 -0500

    Fix formatting issue in cambiarFormatoFecha.java

--- src/cambiarFormatoFecha.java
+++ src/cambiarFormatoFecha.java
@@ -1,10 +1,6 @@
-import java.util.ArrayList;
-import java.util.Scanner;
-
 public class cambiarFormatoFecha {
     public static void main(String[] args) throws Exception {        
         
         System.out.println("Ingrese la fecha en formato dd/mm/aaaa");
     }
 }
-
